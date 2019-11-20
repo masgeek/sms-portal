9c935e74748c017cf374b4975d175241f133aeaf
Sammy Barasa <barsamms@gmail.com>
2019-11-20 11:55:16 +0300

    minor dependency and environment updates

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,10 +13,10 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'Admin Admin',
-            'email' => 'admin@material.com',
+            'name' => 'Sammy B',
+            'email' => 'barsamms@gmail.com',
             'email_verified_at' => now(),
-            'password' => Hash::make('secret'),
+            'password' => Hash::make('andalite6'),
             'created_at' => now(),
             'updated_at' => now()
         ]);
